6503a218558f4f0ee9ccf4d3bc96ae9b59cb7bfb
gayoung106 <xjongamx@kyonggi.ac.kr>
2024-02-04 17:10:58 +0900

    feat: 추가 입력 로직 및 토스트 적용

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpense />
+    <Balance :total="+total" />
+    <IncomeExpense :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -15,7 +15,11 @@ import IncomeExpense from "./components/IncomeExpense.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
+import { useToast } from "vue-toastification";
+
 import { ref, computed } from "vue";
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: "flower", amount: -19.0 },
@@ -30,4 +34,34 @@ const total = computed(() => {
     return acc + transaction.amount;
   }, 0);
 });
+
+//income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+//expeses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  toast.success("성공적으로 추가 됐습니다.");
+};
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,16 +1,55 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="내용을 입력하세요." />
+      <input
+        type="text"
+        id="text"
+        v-model="text"
+        placeholder="내용을 입력하세요."
+      />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="number" id="amount" placeholder="Enter Amount..." />
+      <input
+        type="text"
+        id="amount"
+        v-model="amount"
+        placeholder="Enter Amount..."
+      />
     </div>
+    <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const text = ref("");
+const amount = ref("");
+const emit = defineEmits(["transactionSubmitted"]);
+
+const toast = useToast();
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    toast.error("모두 입력해 주세요.");
+    return;
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+
+  emit("transactionSubmitted", transactionData);
+
+  text.value = "";
+  amount.value = "";
+};
+</script>

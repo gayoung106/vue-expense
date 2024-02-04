f9ff969ded6a0a30f710a199c3a327f306e4e63c
gayoung106 <xjongamx@kyonggi.ac.kr>
2024-02-04 15:34:56 +0900

    feat: 레이아웃 설정

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>Add new transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" placeholder="내용을 입력하세요." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount <br />
+        (negative - expense, positive - income)</label
+      >
+      <input type="number" id="amount" placeholder="Enter Amount..." />
+    </div>
+  </form>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>Expense Tracker by.gazero</h2>
+</template>
+
+<script></script>

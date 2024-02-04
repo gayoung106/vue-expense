f8aa68543844bc817f8e35c65b4ad74fc286ccb4
gayoung106 <xjongamx@kyonggi.ac.kr>
2024-02-04 16:03:44 +0900

    feat: option api 실습적용

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,6 +1,14 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
+    <li
+      v-for="t in transactions"
+      :key="t.id"
+      :class="t.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ t.text }} <span>${{ t.amount }}</span>
+      <button class="delete-btn">X</button>
+    </li>
     <!-- <li class="minus">
       Cash <span>-$400</span> <button class="delete-btn">X</button>
     </li>
@@ -9,3 +17,18 @@
     </li> -->
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "flower", amount: -19.0 },
+        { id: 2, text: "salary", amount: 299.97 },
+        { id: 3, text: "book", amount: -10 },
+        { id: 4, text: "camera", amount: 150 },
+      ],
+    };
+  },
+};
+</script>
